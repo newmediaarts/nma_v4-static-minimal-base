c05f6390a1e0ae39bc40dc9939528eae3576dd92
kccnma <gargiulo@hawaii.edu>
2021-10-27 15:00:36 -1000

    added style guide page and more base styling

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/js');
     eleventyConfig.addPassthroughCopy('src/img');
+    eleventyConfig.addPassthroughCopy('src/video');
 
     return {
         dir: {
